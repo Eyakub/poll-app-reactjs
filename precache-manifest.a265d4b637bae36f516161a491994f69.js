f1794a7698198cd51cccdefe7736a33a1fdf2c12
eyakub <eyakubsorkar@gmail.com>
2020-04-13 22:41:06 +0600

    Updates

---
+++ precache-manifest.a265d4b637bae36f516161a491994f69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4505c9e1076ae1cc02d79bd824cf13",
+    "url": "/poll-app-reactjs/index.html"
+  },
+  {
+    "revision": "3e12819e291db088d3c7",
+    "url": "/poll-app-reactjs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2d1946a1636a3e03f953",
+    "url": "/poll-app-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3e12819e291db088d3c7",
+    "url": "/poll-app-reactjs/static/js/2.dcca35c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/poll-app-reactjs/static/js/2.dcca35c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1946a1636a3e03f953",
+    "url": "/poll-app-reactjs/static/js/main.cd928b59.chunk.js"
+  },
+  {
+    "revision": "b5a483978a6afe769232",
+    "url": "/poll-app-reactjs/static/js/runtime-main.5a172819.js"
+  }
+]);
